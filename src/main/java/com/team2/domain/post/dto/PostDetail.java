85d9427143fa2ff799016b2868a73c9a04b35f64
Zeri-Time <blee.940604@gmail.com>
2025-08-05 16:52:08 +0900

    fix-deleteDTO

--- src/main/java/com/team2/domain/post/dto/PostDetail.java
+++
@@ -1,4 +0,0 @@
-package com.team2.domain.post.dto;
-// detail [id] 명령어로 특정 게시글 내용을 전체 확인
-public class PostDetail {
-}
--- src/main/java/com/team2/domain/post/dto/PostList.java
+++
@@ -1,4 +0,0 @@
-package com.team2.domain.post.dto;
-// list 명령어 입력 시 모든 게시글을 번호순으로 출력
-public class PostList {
-}
